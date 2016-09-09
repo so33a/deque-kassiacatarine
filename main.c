03a5b76eb5ad4a7b33f0b2918a14131bf82090b9
Akira <kassia_catarine.15@hotmail.com>
2016-09-08 22:24:14 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
-#include "fila.h"
+#include "Fila_exercicio.h"
+#include "Fila_exercicio.c"
 
 int main () {
-  FILA alunos = novaFila();
-  inserir(alunos, 9);
-  inserir(alunos, 8);
-  inserir(alunos, 3);
-  inserir(alunos, 10);
+    FILA alunos = novaFila();
+    inserirFinal(alunos, 9);
+    inserirFinal(alunos, 8);
+    inserirInicio(alunos, 2);
+    inserirFinal(alunos, 3);
+    inserirFinal(alunos, 10);
+    imprimirFila(alunos);
+    printf ("removido Inicio: %d \n", removerInicio(alunos));
+    printf ("removido Final: %d \n", removerFinal(alunos));
 
-  imprimirFila(alunos);
+    imprimirFila(alunos);
 
-  while(!filaVazia(alunos))
-    printf ("removido: %d \n", remover(alunos));
+    while(!filaVazia(alunos))
+        printf ("removido: %d \n", remover(alunos));
 
 
-  inserir(alunos, 10);
-  inserir(alunos, 32);
-  
-  imprimirFila(alunos);
+    inserirFinal(alunos, 12);
+    inserirFinal(alunos, 32);
+    inserirInicio(alunos, 22);
+    printf("Nova Fila\n");
+    imprimirFila(alunos);
 
-  destroiFila(alunos);
-  return 0;
+    destroiFila(alunos);
+    return 0;
 }
